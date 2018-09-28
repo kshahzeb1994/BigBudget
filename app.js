d8730259fb3595e3f50eafddec37b72f8ed6d837
kshahzeb1994 <kshahzeb1994@gmail.com>
2018-09-27 19:57:35 -0700

    controller setup

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 var budgetController = (function() {
+    var x = 23;
 
+    var add = function(a) {
+        return x + a;
+    }
+
+    return {
+        publicTest: function(b) {
+            return add(b);
+        }
+    }
 })();
 
 var UIController = (function() {
 
 })();
 
-var controller = (function(budgerCtrl, UICtrl) {
+var controller = (function(budgetCtrl, UICtrl) {
+    // document.querySelector('.add__btn').addEventListener('click', function() {
+    //     console.log("button was clicked.")
+    // });
 
-     
+    var z = budgetCtrl.publicTest(5);
+
+    return {
+        anotherPublic: function() {
+            console.log(z);
+        }
+    }
+
 })(budgetController, UIController);
