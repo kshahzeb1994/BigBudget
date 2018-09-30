0188037d4476641672f38d71654d0439b271bd48
kshahzeb1994 <kshahzeb1994@gmail.com>
2018-09-30 11:16:43 -0700

    Clear fields after input (complete)

--- app.js
+++ app.js
@@ -91,6 +91,19 @@ var UIController = (function() {
 
         },
 
+        clearFields: function() {
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+
+            fieldsArr[0].focus();
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -125,6 +138,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 
         //add the item to UI
         UICtrl.addListItem(newItem, input.type);
+
+        //clear fields
+        UICtrl.clearFields();
 
 
     }
