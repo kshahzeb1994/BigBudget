b6de4cb52d5fb4fd438cb566eae99be50fa7ca1a
kshahzeb1994 <kshahzeb1994@gmail.com>
2018-09-29 21:53:09 -0700

    added pushing entry on data object

--- app.js
+++ app.js
@@ -12,16 +12,42 @@ var budgetController = (function() {
         this.value = value;
     };
 
-    var  data = {
+    var data = {
         allItems: {
-            exp: [],
-            inc: []
+            expense: [],
+            income: []
         },
         totals: {
-            exp: 0,
-            inc: 0
+            expense: 0,
+            income: 0
         }
-    };
+    }
+
+  	return {
+        addItem: function(type, des, val) {
+            alert(type);
+            var newItem, ID;
+
+            if(data.allItems[type].length > 0){
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            if(type === 'expense') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'income') {
+                newItem = new Income(ID, des, val)
+            }
+
+            data.allItems[type].push(newItem);
+            return newItem;
+        },
+        testing: function() {
+            console.log(data);
+            // console.log(newItem);
+        }
+    }
 
 })();
 
@@ -69,12 +95,16 @@ var controller = (function(budgetCtrl, UICtrl) {
     };
 
     var ctrlAddItem = function() {
-        var input = UICtrl.getInput();
-        console.log(input);
+        var input, newItem;
+        //get input data
+        input = UICtrl.getInput();
+
+        //add item to budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
 
     }
 
-    var DOM = UICtrl.getDOMstrings();
 
     return {
         init: function() {
